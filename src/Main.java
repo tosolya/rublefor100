189c53179b628fb8cc5def923bbe6e4a472291a8
Olga Tarasova <bos.olga18@gmail.com>
2021-01-20 10:42:04 +0300

    Changed code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int account = 500;
-        int deposit = 3000;
-        long amount;
+        int deposit = 1300;
         int bonus;
         if (deposit > 1000) {
             bonus = deposit / 100;
-            amount = account + deposit + bonus;
         }
         else { bonus = 0;
-            amount = account + deposit + bonus;
         }
-
             System.out.println(bonus);
-            System.out.println(amount);
       }
 
     }
